070faf25b5edf82bcde6d9f3e9be0361c9e44f42
ISBO1460 <sadati.boina@pole-emploi.fr>
2024-10-23 22:35:14 +0200

    add maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { GoogleMapsModule } from "@angular/google-maps";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GoogleMapsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ftCovoit';
+  title = 'FTCovoit';
+  options: google.maps.MapOptions = {
+    mapId: "53437bfca843bb93",
+    center: { lat: 43.49522719584619, lng: 5.3447904619154 },
+    zoom: 17,
+  };
 }
