c27f7afd37c7186c77ff582ac6c5bc3e30b9a3af
ISBO1460 <sadati.boina@pole-emploi.fr>
2024-10-24 03:39:16 +0200

    add marker and pooler name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,63 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { GoogleMapsModule } from "@angular/google-maps";
+import { GoogleMapsModule,MapMarker} from "@angular/google-maps";
+import {FormsModule} from "@angular/forms";
+import {Pooler} from "../models/pooler";
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GoogleMapsModule],
+  imports: [RouterOutlet,FormsModule,GoogleMapsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'FTCovoit';
+  nomPooler=""
+  addressPooler="";
+  poolers: Pooler[] =
+    [];
   options: google.maps.MapOptions = {
     mapId: "53437bfca843bb93",
     center: { lat: 43.49522719584619, lng: 5.3447904619154 },
-    zoom: 17,
+    zoom: 8
+
   };
+  private pooler!: Pooler;
+
+
+  constructor() {
+
+  }
+
+
+
+  ngOnInit() {
+
+  }
+
+  codeAddress() {
+  let geocoder= new google.maps.Geocoder();
+  this.pooler = {
+    nom: this.nomPooler,
+    adresse: this.addressPooler,
+    lat:0,
+    lng:0
+  }
+  geocoder.geocode({ 'address': this.pooler.adresse},(results:any, status:any)=>{
+      if (status == 'OK') {
+        const location = results[0].geometry.location;
+        this.pooler.lat = location.lat();
+        this.pooler.lng = location.lng();
+        console.log(`Latitude: ${location.lat()}, Longitude: ${location.lng()}`);
+        this.poolers.push(this.pooler);
+        console.log(this.poolers)
+      } else{
+        console.log("progbleme google Maps")
+      }
+    });
+  }
+
 }
---
+++ src/models/pooler.ts
@@ -0,0 +1,7 @@
+export interface Pooler{
+  nom: string,
+  adresse: string,
+  lat: number,
+  lng: number
+}
+
